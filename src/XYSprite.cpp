c791d4c60ff67e15407dbe1ac28fc822cfbc1349
Robin Kirkman <robin.kirkman@gmail.com>
2018-05-15 21:09:39 -0400

    short circuit rendering when possible

--- src/XYSprite.cpp
+++ src/XYSprite.cpp
@@ -11,11 +11,17 @@ XYSprite::XYSprite(int x, int y, RasterMode mode, const Sprite& foreground, cons
 mode_(mode), foreground_(foreground), background_(background), x_(x), y_(y) {}
 
 void XYSprite::render(Sprite::RenderMode render_mode, const Sprite& sprite, int page_num, uint8_t *page) const {
-	int stripe_y = page_num << 3;
+	if (!sprite.raster())
+		return;
+	int page_y = page_num << 3;
+	if (y_ + sprite.height() < page_y || y_ >= page_y + 8)
+		return;
+	if (x_ + sprite.width() < 0 || x_ >= 128)
+		return;
 	int width = sprite.width();
 	if (x_ + width > 128)
 		width = 128 - x_;
-	sprite.render(render_mode, 0, stripe_y - y_, width, page + x_);
+	sprite.render(render_mode, 0, page_y - y_, width, page + x_);
 }
 
 
