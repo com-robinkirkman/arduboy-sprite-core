3b140e99839fe2cccb82472603e94242c8287785
Robin Kirkman <robin.kirkman@gmail.com>
2018-05-15 20:47:45 -0400

    render faster maybe

--- src/SpriteCore.cpp
+++ src/SpriteCore.cpp
@@ -14,12 +14,12 @@ void SpriteCore::begin() {
 	ArduboyCore::boot();
 }
 
-void SpriteCore::display(XYSprite *sprites, size_t len) {
+void SpriteCore::display(const XYSprite *sprites, size_t len) {
 	uint8_t page[kPageWidth];
 	for (size_t n = 0; n < kNumPages; ++n) {
 		memset(page, 0, kPageWidth);
 		for (size_t i = 0; i < len; ++i)
-			sprites[i].render(n, page);
+			(*sprites++).render(n, page);
 		SPI.transfer(page, kPageWidth);
 	}
 }
@@ -28,8 +28,9 @@ void SpriteCore::display(const List<XYSprite>& sprite_list) {
 	uint8_t page[kPageWidth];
 	for (size_t n = 0; n < kNumPages; ++n) {
 		memset(page, 0, kPageWidth);
+		const XYSprite *sprites = &sprite_list[0];
 		for (size_t i = 0; i < sprite_list.size(); ++i) {
-			sprite_list[i].render(n, page);
+			(*sprites++).render(n, page);
 		}
 		SPI.transfer(page, kPageWidth);
 	}
@@ -42,8 +43,9 @@ void SpriteCore::display(const List<List<XYSprite>*>& sprite_lists) {
 		for (size_t i = 0; i < sprite_lists.size(); ++i) {
 			const List<XYSprite> *sprite_list = sprite_lists[i];
 			if (!sprite_list) continue;
+			const XYSprite *sprites = &(*sprite_list)[0];
 			for (size_t j = 0; j < sprite_list->size(); ++j) {
-				(*sprite_list)[j].render(n, page);
+				(*sprites++).render(n, page);
 			}
 		}
 		SPI.transfer(page, kPageWidth);
--- src/SpriteCore.h
+++ src/SpriteCore.h
@@ -17,7 +17,7 @@
 class SpriteCore : public ArduboyCore {
 public:
 	static void begin();
-	static void display(XYSprite* sprites, size_t len);
+	static void display(const XYSprite* sprites, size_t len);
 	static void display(const List<XYSprite>& sprite_list);
 	static void display(const List<List<XYSprite>*>& sprite_lists);
 	static void write(int x, int y, const char *text, XYSprite *sprites);
