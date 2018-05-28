94abd49ac01b74fbe854fbbcba292424ad25a9a0
Robin Kirkman <robin.kirkman@gmail.com>
2018-05-27 20:35:43 -0400

    add spritegfx

---
+++ src/SpriteGfx.cpp
@@ -0,0 +1,25 @@
+/*
+ * SpriteGfx.cpp
+ *
+ *  Created on: May 27, 2018
+ *      Author: robin
+ */
+
+#include <SpriteGfx.h>
+
+int SpriteGfx::bytesRequired(int w, int h) {
+	int rows = h >> 3;
+	if (h & 7) ++rows;
+	return w * rows;
+}
+
+SpriteGfx::SpriteGfx(int w, int h, uint8_t *buf) : Adafruit_GFX(w, h), buf_(buf) {}
+
+void SpriteGfx::drawPixel(int16_t x, int16_t y, uint16_t color) {
+	int row = y >> 3;
+	uint8_t *px = buf_ + row * width() + x;
+	uint8_t mask = 1 << (y & 7);
+	if (color == INVERT) color = (*px & mask) ? BLACK : WHITE;
+	if (color == BLACK) *px &= ~mask;
+	else if(color == WHITE) *px |= mask;
+}
---
+++ src/SpriteGfx.h
@@ -0,0 +1,29 @@
+/*
+ * SpriteGfx.h
+ *
+ *  Created on: May 27, 2018
+ *      Author: robin
+ */
+
+#ifndef SRC_SPRITEGFX_H_
+#define SRC_SPRITEGFX_H_
+
+#include <Adafruit_GFX.h>
+
+class SpriteGfx : public Adafruit_GFX {
+public:
+	static int bytesRequired(int w, int h);
+	static constexpr int BLACK = 0;
+	static constexpr int WHITE = 1;
+	static constexpr int INVERT = 2;
+
+	SpriteGfx(int w, int h, uint8_t *buf);
+	virtual ~SpriteGfx() = default;
+
+	void drawPixel(int16_t x, int16_t y, uint16_t color) override;
+
+private:
+	uint8_t *buf_;
+};
+
+#endif /* SRC_SPRITEGFX_H_ */
